a7370c43beb45dc08ec2d3bce837038dad56664e
Lee Liang <ltliang1@gmail.com>
2018-04-22 20:30:26 -0700

    googleMaps integration

--- milestone3.md
+++ milestone3.md
@@ -1,1 +1,3 @@
 Embed screenshots
+
+Our project is completely different from the idea described in our milestone 2. Per the feedback session we had on Friday, we realized that our previous idea was not clearly connected to the theme--"interacting with real-world data." This is why we changed our project idea to be a web application that collects data from 
--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -5,7 +5,7 @@ function initMap() {
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: ucsd_ltlng,
-		zoom: 15
+		zoom: 18
 	});
 
 	var marker = new google.maps.Marker({
@@ -13,4 +13,32 @@ function initMap() {
 		map:map,
 		title: "HSS"
 	});
+
+	infoWindow = new google.maps.InfoWindow;
+	if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+												'Error: The Geolocation service failed.' :
+												'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
 }
