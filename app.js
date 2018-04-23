06c5ea42170ddcd2df89f3ed126d64f0c61812fc
Lee Liang <ltliang1@gmail.com>
2018-04-22 18:06:33 -0700

    initial node.js setup

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("help me")
+console.log("help me");
+
+// module dependencies
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const handlebars = require('express3-handlebars')
+
+//routes
+const login = require('./routes/login')
+const home = require('./routes/home');
+
+const app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', login.view);
+app.get('/home', home.view);
