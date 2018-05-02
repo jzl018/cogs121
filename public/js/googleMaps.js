b74f6ba238d01a5c973265d1a2ee9d1981832d89
Michelle Nguyen <mnn027@ucsd.edu>
2018-05-02 02:33:02 -0700

    Added some functionality to search bar

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,13 +1,16 @@
 function initMap() {
-	// add your code here
+	//Added this
+	var markers = [];
+
 	var ucsd_ltlng = {lat:32.878733, lng:-117.241694};
 
-	// Create a map object and specify the DOM element for display.
+	//Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: ucsd_ltlng,
 		zoom: 14
 	});
 
+	// Default marker at HSS
 	var marker = new google.maps.Marker({
 		position: ucsd_ltlng,
 		map:map,
@@ -23,7 +26,48 @@ function initMap() {
   	var searchBox = new google.maps.places.SearchBox(
     	/** @type {HTMLInputElement} */
     	(input));
-}
+
+  	// Listen for the event fired when the user selects an item from the
+  	// pick list. Retrieve the matching places for that item.
+  	google.maps.event.addListener(searchBox, 'places_changed', function() {
+    	var places = searchBox.getPlaces();
+
+    	if (places.length == 0) {
+      		return;
+    	}
+    	for (var i = 0, newMarker; newMarker = markers[i]; i++) {
+      		newMarker.setMap(null);
+    	}
+
+    	// For each place, get the icon, place name, and location.
+    	//markers = [];
+    	var bounds = new google.maps.LatLngBounds();
+    	for (var i = 0, place; place = places[i]; i++) {
+      		var image = {
+	        	//url: place.icon,
+	       		size: new google.maps.Size(500, 500),
+	       		origin: new google.maps.Point(0, 0),
+	        	anchor: new google.maps.Point(17, 34),
+	        	scaledSize: new google.maps.Size(35, 35)
+      		};
+
+		    // Create a marker for each place.
+		    var addMarker = new google.maps.Marker({
+		      map: map,
+		      icon: image,
+		      title: place.name,
+		      position: place.geometry.location
+		    });
+
+	      	markers.push(addMarker);
+
+	      	bounds.extend(place.geometry.location);
+	      	bounds.extend(ucsd_ltlng);
+	    };
+    	map.fitBounds(bounds);
+ 	 });
+  	//[END region_getplaces]
+};
 
 // 	infoWindow = new google.maps.InfoWindow;
 // 	if (navigator.geolocation) {
