31a9019533555b5dbf8df71a7a0224b4dce77381
Lee Liang <ltliang1@gmail.com>
2018-05-02 01:13:36 -0700

    changed maps api key

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -5,7 +5,7 @@ function initMap() {
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: ucsd_ltlng,
-		zoom: 18
+		zoom: 14
 	});
 
 	var marker = new google.maps.Marker({
