c9f354e5e8a598d1a26222dc74f7f58e379f983a
Michelle Nguyen <mnn027@ucsd.edu>
2018-05-02 01:05:34 -0700

    Styled search bar

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -18,7 +18,7 @@ function initMap() {
 	// Create the search box and link it to the UI element.
   	var input = /** @type {HTMLInputElement} */ (
     	document.getElementById('pac-input'));
-  	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  	map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
   	var searchBox = new google.maps.places.SearchBox(
     	/** @type {HTMLInputElement} */
