e776400a3a7918c72288c3b45f72f0931e2add7e
Kenneth Truong <ket032@ucsd.edu>
2018-05-16 01:16:06 -0700

    Agenda stuff

--- database.js
+++ database.js
@@ -6,8 +6,8 @@ db.serialize(() => {
   db.run("CREATE TABLE users_to_agenda (name TEXT, coursename TEXT, location TEXT, coursename2 TEXT, location2 TEXT, coursename3 TEXT, location3 TEXT, testcourse TEXT, testlocation TEXT)");
 
   // insert 3 rows of data:
-  db.run("INSERT INTO users_to_agenda VALUES ('michelle', 'COGS 14B', 'PCYN 109', 'COGS 121', 'HSS 1330', 'CSE 100', 'WLH 2001', ' ', 'location1')");
-  db.run("INSERT INTO users_to_agenda VALUES ('kenneth', 'COGS 121', 'HSS 1330', 'COGS 107C', 'PETER 108', 'COGS 101C', 'PETER 110', 'course2', 'location2')");
+  db.run("INSERT INTO users_to_agenda VALUES ('Events', 'COGS 14B', 'PCYN 109', 'COGS 121', 'HSS 1330', 'CSE 100', 'WLH 2001', ' ', 'location1')");
+  //db.run("INSERT INTO users_to_agenda VALUES ('kenneth', 'COGS 121', 'HSS 1330', 'COGS 107C', 'PETER 108', 'COGS 101C', 'PETER 110', 'course2', 'location2')");
 
   console.log('successfully created the users_to_agenda table in agenda.db');
 
