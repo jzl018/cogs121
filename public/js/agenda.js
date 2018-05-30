c789341681e39e6c1afc9b6e08c764ff74471d3d
Michelle Nguyen <mnn027@ucsd.edu>
2018-05-30 04:07:17 -0700

    Location box in agenda takes in actual location

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -69,13 +69,13 @@ $('#Events').click(function() {
       type: 'POST', // <-- this is POST, not GET
       data: {
               name: $('#insertNameBox').val(),
-              location: $('#insertLocationBox').val()
+              location: autocomplete.getPlace().name
             },
       success: (data) => {
         calledFromOther = true;
         $('#Events').trigger('click');
         $('#insertNameBox').val('');
-        $('#insertLocationBox').val('');
+        $('#autocomplete').val('');
       }
     });
   });
@@ -92,7 +92,38 @@ $('#Events').click(function() {
         calledFromOther = true;
         $('#Events').trigger('click');
         $('#insertNameBox').val('');
-        $('#insertLocationBox').val('');
+        $('#autocomplete').val('');
       }
     });
   });
+
+  var placeSearch, autocomplete, geocoder;
+
+function initAutocomplete() {
+  geocoder = new google.maps.Geocoder();
+  autocomplete = new google.maps.places.Autocomplete(
+    (document.getElementById('autocomplete')), {
+      types: ['geocode']
+    });
+
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function codeAddress(address) {
+  geocoder.geocode({
+    'address': address
+  }, function(results, status) {
+    if (status == 'OK') {
+      // This is the lat and lng results[0].geometry.location
+      alert(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function fillInAddress() {
+  var place = autocomplete.getPlace();
+
+  //codeAddress(document.getElementById('autocomplete').value);
+}
