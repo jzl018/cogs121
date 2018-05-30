78ef2a3c8082f9a4f5b5e3ac2c308a13421243e0
Lee Liang <ltliang1@gmail.com>
2018-05-30 05:27:51 -0700

    agenda styling and bug fixes

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,104 +1,67 @@
-let calledFromOther = false;
+$(document).ready(() => {
+  reloadAgenda();
+});
 
-$('#Events').click(function() {
-  const eventsButton = $("#Events");
-  let buttonText = eventsButton.text();
-
-  //const title = $('#Events.studentBtn btn btn-dark');
-  //console.log(title);
-/*  console.log('making ajax request to:', requestURL);
-
+function reloadAgenda() {
   $.ajax({
-    url: requestURL,
+    url: 'users',
     type: 'GET',
     dataType: 'json',
     success: (data) => {
-      console.log('You received some data!', data);
-      $('#class1').html(data.coursename);
-      $('#location1').html(data.location);
-      $('#class2').html(data.coursename2);
-      $('#location2').html(data.location2);
-      $('#class3').html(data.coursename3);
-      $('#location3').html(data.location3);
-      $('#class4').html(data.testcourse);
-      $('#location4').html(data.testlocation);*/
+      $('#agenda').html(data);
+      $('#agenda').show();
+    },
+  });
+}
+
+$('#insertButton').click(() => {
+  console.log($('#autocomplete').text());
 
 
-  if (buttonText=="See My Events") {
-    eventsButton.html("My Events");
-    $.ajax({
-      url: 'users',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        $('#agenda').html(data);
-        $('#agenda').show();
-      },
-    });
-  } else {
-    if (!calledFromOther) {
-      eventsButton.html("See My Events");
-      $('#agenda').hide();
+  // const eventsButton = $("#Events");
+  // let buttonText = eventsButton.text();
+
+  $.ajax({
+    // all URLs are relative to http://localhost:3000/
+    url: 'users',
+    type: 'POST', // <-- this is POST, not GET
+    data: {
+            name: $('#insertNameBox').val(),
+            location: autocomplete.getPlace().name
+          },
+    success: (data) => {
+
+      reloadAgenda();
+
+      // calledFromOther = true;
+      // $('#Events').trigger('click');
+      $('#insertNameBox').val('');
+      $('#autocomplete').val('');
     }
-    else {
-      $.ajax({
-        url: 'users',
-        type: 'GET',
-        dataType: 'json',
-        success: (data) => {
-          $('#agenda').html(data);
-          $('#agenda').show();
-        },
-      });
-      calledFromOther = false;
-    }
-  }
+  });
 });
 
+$('#deleteButton').click(() => {
 
+  $.ajax({
+    url: 'users/Delete',
+    type: 'POST',
+    data: {
+      name: $('#insertNameBox').val(),
+    },
+    success: (data) => {
 
-  $('#insertButton').click(() => {
-    console.log($('#autocomplete').text());
+      reloadAgenda();
 
-
-    const eventsButton = $("#Events");
-    let buttonText = eventsButton.text();
-
-    $.ajax({
-      // all URLs are relative to http://localhost:3000/
-      url: 'users',
-      type: 'POST', // <-- this is POST, not GET
-      data: {
-              name: $('#insertNameBox').val(),
-              location: autocomplete.getPlace().name
-            },
-      success: (data) => {
-        calledFromOther = true;
-        $('#Events').trigger('click');
-        $('#insertNameBox').val('');
-        $('#autocomplete').val('');
-      }
-    });
+      // calledFromOther = true;
+      // $('#Events').trigger('click');
+      $('#insertNameBox').val('');
+      $('#autocomplete').val('');
+    }
   });
+});
 
-  $('#deleteButton').click(() => {
-
-    $.ajax({
-      url: 'users/Delete',
-      type: 'POST',
-      data: {
-        name: $('#insertNameBox').val(),
-      },
-      success: (data) => {
-        calledFromOther = true;
-        $('#Events').trigger('click');
-        $('#insertNameBox').val('');
-        $('#autocomplete').val('');
-      }
-    });
-  });
-
-  var placeSearch, autocomplete, geocoder;
+var placeSearch, autocomplete, geocoder;
 
 function initAutocomplete() {
   geocoder = new google.maps.Geocoder();
@@ -133,3 +96,60 @@ function fillInAddress() {
 
   //codeAddress(document.getElementById('autocomplete').value);
 }
+
+// let calledFromOther = false;
+
+// $('#Events').click(function() {
+//   const eventsButton = $("#Events");
+//   let buttonText = eventsButton.text();
+
+  //const title = $('#Events.studentBtn btn btn-dark');
+  //console.log(title);
+/*  console.log('making ajax request to:', requestURL);
+
+  $.ajax({
+    url: requestURL,
+    type: 'GET',
+    dataType: 'json',
+    success: (data) => {
+      console.log('You received some data!', data);
+      $('#class1').html(data.coursename);
+      $('#location1').html(data.location);
+      $('#class2').html(data.coursename2);
+      $('#location2').html(data.location2);
+      $('#class3').html(data.coursename3);
+      $('#location3').html(data.location3);
+      $('#class4').html(data.testcourse);
+      $('#location4').html(data.testlocation);*/
+
+
+  // if (buttonText=="See My Events") {
+  //   eventsButton.html("My Events");
+  //   $.ajax({
+  //     url: 'users',
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     success: (data) => {
+  //       $('#agenda').html(data);
+  //       $('#agenda').show();
+  //     },
+  //   });
+  // } else {
+  //   if (!calledFromOther) {
+  //     eventsButton.html("See My Events");
+  //     $('#agenda').hide();
+  //   }
+  //   else {
+  //     $.ajax({
+  //       url: 'users',
+  //       type: 'GET',
+  //       dataType: 'json',
+  //       success: (data) => {
+  //         $('#agenda').html(data);
+  //         $('#agenda').show();
+  //       },
+  //     });
+  //     calledFromOther = false;
+  //   }
+  // }
+// });
